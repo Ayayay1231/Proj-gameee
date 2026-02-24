0c32101bab0c4bfe750e8e9c0fe111669cf68b56
Ayayay1231 <champ.25752@gmail.com>
2026-02-24 13:41:25 +0700

    Enhance Monster class with SFML graphics support
    Added texture and sprite handling for the Monster class.

--- header/Monster.h
+++ header/Monster.h
@@ -1,5 +1,7 @@
 #ifndef MONSTER_H
 #define MONSTER_H
+#include <SFML/Graphics.hpp>
+#include <iostream>
 #include <string>
 using namespace std;
 
@@ -7,12 +9,27 @@ class Monster {
 public:
     string name;
     int hp, maxHp, maxDmg; // เพิ่ม maxHp เข้าไปตรงนี้
+    sf::Texture texture;
+    sf::Sprite sprite;
+
 
     Monster(int rank) {
         name = "Monster #" + to_string(rank);
         maxHp = 50 + (rank * 10);
         hp = maxHp;
         maxDmg = 5 + (rank * 2);
+
+        int imageIndx =  1;
+        string imgpath = "monster" + to_string(imageIndx) + ".png" ;
+
+        if(!texture.loadFromFile(imgpath)){
+            cout << "cannot load " << imgpath << endl;
+
+        }else{
+            sprite.setTexture(texture);
+            sprite.setPosition(500.f,150.f);
+            
+        }
     }
 };
 #endif
